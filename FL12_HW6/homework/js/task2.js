2962be8ecb2727aa4512411747887e8ba2ae18d1
Maksym Polinka <Polinka7max@gmail.com>
2019-12-24 10:46:44 +0200

    Completed HW6 assignment

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,54 +1,21 @@
-// Your code goes here
+let a = prompt('Please, enter the length of the a side of the triangle: ')
+let b = prompt('Please, enter the length of the b side of the triangle: ')
+let c = prompt('Please, enter the length of the c side of the triangle: ')
 
-/*
-Task 2. Identify triangle type.
-	App workflow:
-1.	User input 3 numbers (a, b, c) for triangle sides length.
-2.	Check type of inputs. If type of inputs ( a OR b OR c ) is: empty string, undefined -  show modal window with message ( input values should be ONLY numbers )
-3.	Check values of input values. If one of the value ( a OR b OR c ) equal to 0 - show modal window with message ( A triangle must have 3 sides with a positive definite length )
-4.	Check such triangle can exist. If it can’t exit show modal window with message -  ‘Triangle doesn’t exist’.
-5.	If triangle exist check its type: 
-●	equivalent (every side is equal)
-●	isosceles (two sides are equal) 
-●	scalene (no sides are equal)
-6.	Use console.log to show result, variants:
-
--	If such triangle can’t exist: ‘Triangle doesn’t exist’
--	If triangle has three equal sides: ‘Equilateral triangle’
--	If triangle has two equal sides, third differs: ‘Isosceles triangle’
--	If triangle has three different sides: ‘Scalene triangle’
-
-*/
-
-// −	Use prompt for handle user input and alert for error message.
-
-//TODO: make the programme ask for input until it's given a correct one 
-
-var a = prompt("Please, enter the length of the a side of the triangle: ")
-var b = prompt("Please, enter the length of the b side of the triangle: ")
-var c = prompt("Please, enter the length of the c side of the triangle: ")
-
-if (a == null && a == '' || b == null && b == '' || c == null && c == '') {
-    alert("Input values should be ONLY numbers")
+if (a === null && a === '' || b === null && b === '' || c === null && c === '') {
+    alert('Input values should be ONLY numbers')
 }
 
-if (a == 0 || b == 0 || c == 0) {
-    alert("A triangle must have 3 sides with a positive definite length")
+if (a === 0 || b === 0 || c === 0) {
+    alert('A triangle must have 3 sides with a positive definite length')
 }
 
 if (!(c + b > a || a + c > b || a + b > c)) {
-    alert("Triangle doesn’t exist")
-} else if (a == b && a == c && b == c) {
-    console.log("Equilateral triangle")
-} else if (a == b && a == c && b == c || a == b && a != c && b == c || a != b && a == c && b == c) { //проблема тут: everything works except this part 
-    console.log("Isosceles triangle")
-} else if (a != b && a != c && b != c) {
-    console.log("Scalene triangle")
+    alert('Triangle doesn’t exist')
+} else if (a === b && a === c && b === c) {
+    console.log('Equilateral triangle')
+} else if (a === b && a === c && b === c || a === b && a !== c && b === c || a !== b && a === c && b === c) {
+    console.log('Isosceles triangle')
+} else if (a !== b && a !== c && b !== c) {
+    console.log('Scalene triangle')
 }
-
-/*
--	If such triangle can’t exist: ‘Triangle doesn’t exist’
--	If triangle has three equal sides: ‘Equilateral triangle’
--	If triangle has two equal sides, third differs: ‘Isosceles triangle’
--	If triangle has three different sides: ‘Scalene triangle’
-*/
