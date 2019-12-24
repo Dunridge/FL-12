7a4d9be93a1ea26bed625f817de40c0baa24c329
Maksym Polinka <Polinka7max@gmail.com>
2019-12-24 14:14:43 +0200

    Completed HW6 with corrections

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,18 +2,18 @@ let a = prompt('Please, enter the a coeficient of the quadratic equation: ')
 let b = prompt('Please, enter the b coeficient of the quadratic equation: ')
 let c = prompt('Please, enter the c coeficient of the quadratic equation: ')
 
+let raise_to_power = 2
+let discrim_coef = 4
+let div_by_two = 2
+
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('Invalid input data')
 }
 
-let to_power = 2
-let discr_coef = 4
-let divide_by = 2
-
-let D = Math.pow(b, to_power) - discr_coef * a * c
-let x = -b / divide_by * a
-let x1 = (-b + Math.sqrt(D)) / divide_by * a
-let x2 = (-b - Math.sqrt(D)) / divide_by * a
+let D = Math.pow(b, raise_to_power) - discrim_coef * a * c
+let x = -b / div_by_two * a
+let x1 = (-b + Math.sqrt(D)) / div_by_two * a
+let x2 = (-b - Math.sqrt(D)) / div_by_two * a
 
 if (D >= 0) {
     if (D === 0) {
